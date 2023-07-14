4505246d8b22a85d64dd23b90b728315e6b0d072
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-07-14 11:10:19 +0900

    feat: refetch feature

--- src/features/photo/hooks/usePhotos.tsx
+++ src/features/photo/hooks/usePhotos.tsx
@@ -2,7 +2,10 @@ import { useQuery } from 'react-query'
 import { photosService } from '..'
 
 const usePhotos = () => {
-  const { data } = useQuery('/user', photosService, { suspense: true })
+  const { data } = useQuery('/user', photosService, {
+    suspense: true,
+    refetchInterval: 60 * 1000,
+  })
 
   return {
     data,
